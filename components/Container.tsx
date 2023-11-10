84f1b698e17dcbab5ea2c3870bb8fe7c566e8924
h12j21-star <jh06079@naver.com>
2023-11-10 14:05:02 +0900

    글꼴 추가

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,22 +1,30 @@
-import Head from 'next/head';
 import Nav from './Nav';
 import Footer from './Footer';
 import SEO from './SEO';
 import style from '@/styles/layout.module.css';
+import Link from 'next/link';
+import localFont from 'next/font/local';
+
+const myFont = localFont({
+  src: '../utils/PretendardVariable.ttf',
+});
+
 export default function Container(props) {
   //const title = props.post.title || 'My Blog';
   const title = 'My Blog';
   return (
-    <>
+    <div className={myFont.className}>
       <div className={style.box}>
         <SEO title={title} />
         <header className={style.header}>
-          <h1 className={style.header_title}>임디디</h1>
+          <Link href={'/'}>
+            <h1 className={style.header_title}>임디디</h1>
+          </Link>
           <Nav />
         </header>
         <main>{props.children}</main>
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
