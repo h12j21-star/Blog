22799b11eeee67b1e072e3f266707088e85907bc
h12j21-star <jh06079@naver.com>
2023-11-16 16:53:05 +0900

    페이지 전환 애니메이션 삭제

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,7 +4,6 @@ import SEO from './SEO';
 import style from '@/styles/layout.module.css';
 import Link from 'next/link';
 import localFont from 'next/font/local';
-import { Transition, TransitionGroup } from 'react-transition-group';
 import { useRouter } from 'next/router';
 
 const myFont = localFont({
@@ -13,22 +12,6 @@ const myFont = localFont({
     { path: '../utils/Pretendard-Light.otf', style: 'light' },
   ],
 });
-
-// const TIMEOUT = 400;
-// const getTransitionStyles = {
-//   entering: {
-//     position: `absolute`,
-//     opacity: 0,
-//   },
-//   entered: {
-//     transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
-//     opacity: 1,
-//   },
-//   exiting: {
-//     transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
-//     opacity: 0,
-//   },
-// };
 
 export default function Container(props) {
   const title = props.title || '오늘 공부 기록';
@@ -49,26 +32,4 @@ export default function Container(props) {
       </div>
     </div>
   );
-}
-
-{
-  /* <TransitionGroup style={{ position: 'relative' }}>
-<Transition
-  key={router.pathname}
-  timeout={{
-    enter: TIMEOUT,
-    exit: TIMEOUT,
-  }}
->
-  {(status) => (
-    <main
-      style={{
-        ...getTransitionStyles[status],
-      }}
-    >
-      {props.children}
-    </main>
-  )}
-</Transition>
-</TransitionGroup> */
 }
