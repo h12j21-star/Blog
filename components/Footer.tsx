53a5d3327ad80107336f471481ee35ea60a1faef
h12j21-star <jh06079@naver.com>
2023-12-11 16:25:00 +0900

    footer git 링크 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,23 @@
 import Image from 'next/image';
 
 import style from '@/styles/layout.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <>
       <div className={style.footer}>
         <div className={style.footerBox}>
-          <Image
-            src="/github-mark.png"
-            alt="깃허브 이동"
-            width={30}
-            height={30}
-            className={style.footer_GitIcon}
-          />
-          <p className={style.copyRight}>Copyright © 2023 Choi heyeonji</p>
+          <Link href={'https://github.com/h12j21-star/Blog'}>
+            <Image
+              src="/github-mark.png"
+              alt="깃허브 이동"
+              width={30}
+              height={30}
+              className={style.footer_GitIcon}
+            />
+          </Link>
+          <p className={style.copyRight}>Copyright © 2023 Choi hyeonji</p>
         </div>
       </div>
     </>
